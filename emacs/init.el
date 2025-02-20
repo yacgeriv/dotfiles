efbce4e8f56a346e317a7987b515d4ead0e44bf8
yacodin <sinyax75@gmail.com>
2025-02-21 01:03:20 +0400

    new changes

--- emacs/init.el
+++ emacs/init.el
@@ -1,6 +1,6 @@
 (setq make-backup-files nil)
 (set-frame-font "Iosevka 15")
-(load-theme 'minimal t)
+(load-theme 'gruber-darker t)
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 
@@ -51,3 +51,16 @@
 (add-hook 'c++-mode-hook 'display-line-numbers-mode)
 (add-hook 'c-mode-hook 'display-line-numbers-mode)
 (setq linum-format "%3d ")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(gruber-darker-theme yasnippet which-key projectile minimal-theme lsp-ui inkpot-theme helm-xref helm-lsp flycheck eziam-themes dap-mode company catppuccin-theme badwolf-theme badger-theme almost-mono-themes ahungry-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
